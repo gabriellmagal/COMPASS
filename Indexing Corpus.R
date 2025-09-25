4c64767a82c45866f7e235acfde27f9098ec834b
Gabriel Magalhães <124104310+gabriellmagal@users.noreply.github.com>
2025-09-24 22:25:10 -0300

    Update Indexing Corpus.R

--- Indexing Corpus.R
+++ Indexing Corpus.R
@@ -1,3 +1,19 @@
+# COMPASS: Corpus Mapper for Phonetical And Syllabic Structures
+# Copyright (C) 2025  Gabriel Magalhães da Silveira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # --- PRÉ-PROCESSAMENTO ---
 
 # --- BIBLIOTECAS E MODELO ---
@@ -177,5 +193,6 @@ base_de_dados_final <- bind_rows(lista_de_dataframes_fones)
 
 cat(sprintf("Saving the final database in: %s\n", caminho_saida_rds))
 saveRDS(base_de_dados_final, file = caminho_saida_rds)
+
 
 cat("\n--- PRE-PROCESSING COMPLETE! ---\n")
