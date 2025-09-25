f31c61ee9581503eb82a1d081a04c55e6a25259a
Gabriel Magalhães <124104310+gabriellmagal@users.noreply.github.com>
2025-09-24 22:24:37 -0300

    Update Shinyapp.R

--- Shinyapp.R
+++ Shinyapp.R
@@ -1,3 +1,19 @@
+# COMPASS: Corpus Mapper for Phonetical And Syllabic Structures
+# Copyright (C) 2025  Gabriel Magalhães da Silveira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # --- BIBLIOTECAS ---
 if (!requireNamespace("shiny", quietly = TRUE)) install.packages("shiny")
 library(shiny)
@@ -591,3 +607,4 @@ server <- function(input, output, session) {
 
 # --- APPSHINY ---
 shinyApp(ui, server)
+
