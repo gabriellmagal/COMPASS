3342d79a577093c1bee58d666aca6e29e51e7941
Gabriel Magalhães <124104310+gabriellmagal@users.noreply.github.com>
2025-11-11 20:38:47 -0300

    Update Syllabifier.R

--- Syllabifier.R
+++ Syllabifier.R
@@ -40,7 +40,7 @@ ATAQUES_VALIDOS <- c(
   # Ataques Complexos (CC)
   "pl","bl","kl","ɡl","fl","vl","pɾ","bɾ","tɾ","dɾ","kɾ","ɡɾ","fɾ","vɾ",
   # Ataques Complexos (CG) - Consoante + Glide
-  "kw", "ɡw", "bj", "pj", "kj", "ɡj", "mj", "nj", "lj", "fj", "vj", "sj", "zj", "ʃj", "ʒj"
+  "kw", "ɡw", "bj", "pj", "kj", "ɡj", "mj", "nj", "lj", "fj", "vj", "sj", "zj", "ʃj", "ʒj",
   # Ataques Complexos (CCG) - Consoante + Consoante + Glide
   "pɾj", "bɾj", "tɾj", "dɾj", "kɾj", "ɡɾj", "fɾj", "vɾj", "plj", "blj", "klj", "ɡlj", "flj", "vlj"
 )
@@ -243,5 +243,6 @@ for (nome_arquivo_base in arquivos_tg_nomes) {
 
 
 cat("\n--- Process completed! ---\n")
+
 
 
